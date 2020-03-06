bac9fcfd9f315187d6df43fd6766a91256035763
Jildert Venema <j.venema@ziuz.com>
2020-03-06 12:11:13 +0100

    follow auto

---
+++ precache-manifest.b67b00d139066f54474586aebef5e266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3397925c70a2142ff35c3c0e55130b5e",
+    "url": "/spotify-friends-app/index.html"
+  },
+  {
+    "revision": "9b6a713b5a9fc422d9ac",
+    "url": "/spotify-friends-app/static/css/2.609d5137.chunk.css"
+  },
+  {
+    "revision": "7856db04ce83e493545d",
+    "url": "/spotify-friends-app/static/css/main.28415029.chunk.css"
+  },
+  {
+    "revision": "9b6a713b5a9fc422d9ac",
+    "url": "/spotify-friends-app/static/js/2.dd574d1f.chunk.js"
+  },
+  {
+    "revision": "7856db04ce83e493545d",
+    "url": "/spotify-friends-app/static/js/main.0db5f8df.chunk.js"
+  },
+  {
+    "revision": "0f0d79cd6891cf4435b7",
+    "url": "/spotify-friends-app/static/js/runtime~main.ff025761.js"
+  }
+]);
